6a84b39d705cad331be84170760aff2c1a85f37e
Adishankar-H <official.adishankar@gmail.com>
2024-11-28 09:55:47 +0530

    First Commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from './chai.jsx'
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <div>
+        <h3>Coffee</h3>
+        <p>coffee is good</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,10 @@
+function Chai() {
+    return (
+        <>
+            <h1>Chai aur react</h1>
+            <p>Chai makes life interesting.</p>
+        </>
+    )
+}
+
+export default Chai;
